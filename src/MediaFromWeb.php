7d08562b4498e96116ae7e0ada1e1c1923997e12
José Emmanuel Lozoya <jose.emmanuel.lozoya@gmail.com>
2015-09-10 21:54:50 -0500

    Return an error due to an unsuccessful request.

--- src/MediaFromWeb.php
+++ src/MediaFromWeb.php
@@ -3,6 +3,26 @@ namespace Elozoya\MediaFromWeb;
 
 class MediaFromWeb
 {
+    private $httpClient;
 
+    public function __construct(\GuzzleHttp\Client $httpClient)
+    {
+        $this->httpClient = $httpClient;
+    }
+
+    public function getPhotosFromUrl($url)
+    {
+        $photos = [];
+        $result = (object)[
+          "data" => $photos,
+        ];
+        $response = $this->httpClient->request('HEAD', $url);
+        if (!$response->isSuccessful()) {
+            return (object)[
+                "error" => true,
+                'message' => "Photos not found or you are not allowed to get them",
+            ];
+        }
+        return $result;
+    }
 }
-
--- tests/MediaFromWebTest.php
+++ tests/MediaFromWebTest.php
@@ -1,5 +1,6 @@
-<?php
+<?php namespace Elozoya\MediaFromWeb\Unit;
 
+use PHPUnit_Framework_TestCase;
 use Mockery as m;
 use Elozoya\MediaFromWeb\MediaFromWeb;
 
@@ -10,5 +11,18 @@ class MediaFromWebTest extends PHPUnit_Framework_TestCase
         m::close();
     }
 
+    public function testGetPhotosReturnsAnErrorResultDueToAnUnsuccessfulRequest()
+    {
+        $mockHttpClient = m::mock('\GuzzleHttp\Client');
+        $mockResponse = m::mock('StdClass');
+        $mediaFromWeb = new MediaFromWeb($mockHttpClient);
+        $url = "http://foo.com/bar.png";
+        $mockHttpClient->shouldReceive("request")->once()->with('HEAD', $url)->andReturn($mockResponse);
+        $mockResponse->shouldReceive('isSuccessful')->once()->andReturn(false);
+        $result = $mediaFromWeb->getPhotosFromUrl($url);
+        $this->assertEquals($result, (object)[
+          "error" => true,
+          'message' => "Photos not found or you are not allowed to get them",
+        ]);
+    }
 }
-
